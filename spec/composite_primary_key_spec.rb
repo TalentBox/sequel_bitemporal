d65322bfcb1560ec9aa2faeb74df2ba15b0863ee
DenisKem <denis.kem@gmail.com>
2019-01-24 13:21:28 +0300

    specs for versions working with composite key

--- spec/composite_primary_key_spec.rb
+++ spec/composite_primary_key_spec.rb
@@ -14,4 +14,38 @@ describe "composite_primary_key" do
       end.to raise_error Sequel::Error, ERROR_TEXT
     end
   end
+
+  describe "versions work correctly" do
+    BT = Sequel::Plugins::Bitemporal
+
+    let(:master) { @master_class.new(name: "john Smith") }
+    let(:junior) { "Junior Ruby-developer" }
+    let(:senior) { "Senior Ruby-developer" }
+    let(:two_years_in_seconds) { 2 * 365 * 24 * 60 * 60 }
+
+    before do
+      setup_composite_primary_key
+      master.department_id = 1
+      master.team_id = 1
+      master.save
+
+      BT.at(Time.now) do
+        master.update_attributes(position: junior)
+      end
+
+      BT.at(Time.now + two_years_in_seconds) do
+        master.update_attributes(position: senior)
+      end
+    end
+
+    specify do
+      BT.at(Time.now) do
+        expect(master.reload.position).to eq(junior)
+      end
+
+      BT.at(Time.now + two_years_in_seconds) do
+        expect(master.reload.position).to eq(senior)
+      end
+    end
+  end
 end
